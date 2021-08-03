8eab049d2af521c9634f632da52240b417fe710a
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-08-04 03:18:44 +0600

    Recursive Comment Add

--- factorial-recursive.js
+++ factorial-recursive.js
@@ -1,3 +1,18 @@
+// Factorial
+// 1! = 1
+// 2! = 2 X 1
+// 3! = 3 X 2 X 1
+// 4! = 4 X 3 X 2 X 1
+// 5! = 5 X 4 X 3 X 2 X 1
+
+// Recursive Factorial
+// 1! = 1
+// 2! = 2 X 1!
+// 3! = 3 X 2!
+// 4! = 4 X 3!
+// 5! = 5 X 4!
+
+
 // Define Function For Factorial
 function factorial(n) {
     if (n === 1) {
